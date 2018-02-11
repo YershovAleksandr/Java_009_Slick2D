112b0192a0793d323c30245e482554f4ba6efb20
hp <namstudionsk@gmail.com>
2018-02-11 23:02:50 +0700

    Fix lv

--- src/com/nam/GameApp.java
+++ src/com/nam/GameApp.java
@@ -8,12 +8,19 @@ public class GameApp extends BasicGame{
 
     private Logger log = LoggerFactory.getLogger(GameApp.class);
 
+    private Image img;
+
     GameApp(String gamename){
         super(gamename);
     }
 
     public void init(GameContainer gc) throws SlickException{
         log.info("Init");
+
+        XMLPackedSheet sheet = new XMLPackedSheet("res/art/sheet.png", "res/art/sheet.xml");
+
+        img = sheet.getSprite("buttonRed");
+
     }
 
     public void update(GameContainer gc, int i) throws SlickException{
@@ -23,5 +30,9 @@ public class GameApp extends BasicGame{
     public void render(GameContainer gc, Graphics g) throws SlickException{
         g.setColor(Color.red);
         g.drawRect(100, 100, 300, 300);
+
+        img.draw(100, 100);
+
+        //g.drawImage(img, 100, 100);
     }
 }
